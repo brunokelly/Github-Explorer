306ebe9cfe1c46ab5ef507c0f43b7d53eaf9756d
Bruno Kelly <brunobuch00@gmail.com>
2021-06-06 11:29:14 -0300

    Estilizando Dashboard

---
+++ src/assets/img.ts
@@ -0,0 +1,5 @@
+export const logoImg =
+  "https://xesque.rocketseat.dev/platform/1587379765556-attachment.svg";
+
+export const githubBackground =
+  "https://xesque.rocketseat.dev/platform/1587379725719-attachment.svg";
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React from "react";
+import { FiChevronRight } from "react-icons/fi";
+
+import { Title, Form, Repositories } from "./style";
+
+import { logoImg } from "../../assets/img";
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/45316003?v=4"
+            alt="Bruno Vieira"
+          />
+
+          <div>
+            <strong>rockseat/unform/</strong>
+            <p>Teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
